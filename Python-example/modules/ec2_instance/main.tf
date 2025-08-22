f6824c2b8ab9b1103a99286a1c9006ad7c6d5623
sravanchekurthi <srsudden@gmail.com>
2025-08-22 11:18:22 -0500

    Updating with comments

--- Python-example/modules/ec2_instance/main.tf
+++ Python-example/modules/ec2_instance/main.tf
@@ -11,21 +11,22 @@ resource "aws_instance" "sampleinstance" {
 
   connection {
     type = "ssh"
-    user = "ubuntu"
-    private_key = file("~/.ssh/id_rsa")
+    user = "ubuntu"  # Replace with the appropriate username for your EC2 instance
+    private_key = file("~/.ssh/id_rsa")  # Replace with the path to your private key
     host = self.public_ip
   }
 
+  # File provisioner to copy a file from local to the remote EC2 instance
   provisioner "file" {
-    source = "app.py"
-    destination = "/home/ubuntu/appy.py"
+    source = "app.py"  # Replace with the path to your local file
+    destination = "/home/ubuntu/appy.py" # Replace with the path on the remote instance
   }
 
   provisioner "remote-exec" {
     inline = [ 
         "echo 'Hello from remote instance'",
-        "sudo apt update -y",
-        "sudo apt-get install -y python3-pip",
+        "sudo apt update -y",  # Update package lists (for ubuntu)
+        "sudo apt-get install -y python3-pip",  # Example package installation
         "cd /home/ubuntu",
         "sudo pip3 install flask",
         "sudo python3 app.py &"
--- Python-example/modules/key_pair/main.tf
+++ Python-example/modules/key_pair/main.tf
@@ -1,8 +1,8 @@
 provider "aws" {
-  region = "us-east-1"
+  region = "us-east-1"  # Replace with your desired AWS region.
 }
 
 resource "aws_key_pair" "sample" {
-  key_name = var.key_pair_name
-  public_key = file("~/.ssh/id_rsa.pub")
+  key_name = var.key_pair_name    # Replace with your desired key name
+  public_key = file("~/.ssh/id_rsa.pub")  # Replace with the path to your public key file
 }
